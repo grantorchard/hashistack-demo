5544ef910e9a04ee8de0c717bcacbe924c76f298
Grant Orchard <go@hashicorp.com>
2019-10-31 11:49:43 +1100

    Updated to create certs for both tfe and gitlab

--- custom_cert/main.tf
+++ custom_cert/main.tf
@@ -2,19 +2,38 @@ provider "acme" {
   server_url = var.server_url
 }
 
-resource "tls_private_key" "private_key" {
+resource "tls_private_key" "tfe" {
   algorithm = "RSA"
 }
 
 resource "acme_registration" "tfe" {
-  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
+  account_key_pem = "${tls_private_key.tfe.private_key_pem}"
   email_address   = var.email_address
 }
 
-resource "acme_certificate" "certificate" {
+resource "acme_certificate" "tfe" {
   account_key_pem           = "${acme_registration.tfe.account_key_pem}"
-  common_name               = var.common_name
-  subject_alternative_names = var.subject_alternative_names
+  common_name               = var.tfe_common_name
+  subject_alternative_names = var.tfe_subject_alternative_names
+
+  dns_challenge {
+    provider = "route53"
+  }
+}
+
+resource "tls_private_key" "gitlab" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "gitlab" {
+  account_key_pem = "${tls_private_key.gitlab.private_key_pem}"
+  email_address   = var.email_address
+}
+
+resource "acme_certificate" "gitlab" {
+  account_key_pem           = "${acme_registration.gitlab.account_key_pem}"
+  common_name               = var.gitlab_common_name
+  subject_alternative_names = var.gitlab_subject_alternative_names
 
   dns_challenge {
     provider = "route53"
--- custom_cert/variables.tf
+++ custom_cert/variables.tf
@@ -7,14 +7,20 @@ variable "email_address" {
     type = "string"
 }
 
-variable "common_name" {
+variable "tfe_common_name" {
     type = "string"
 }
 
-variable "subject_alternative_names" {
+variable "tfe_subject_alternative_names" {
   type = "list"
   default = []
 }
 
+variable "gitlab_common_name" {
+    type = "string"
+}
 
-
+variable "gitlab_subject_alternative_names" {
+  type = "list"
+  default = []
+}
